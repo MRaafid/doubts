d66946c3430c7c092945c5292aa328a043bcfb52
Muhammad Raafid <140726193+MRaafid@users.noreply.github.com>
2023-10-05 19:52:19 +0530

    Create 4.js

---
+++ 4.js
@@ -0,0 +1,32 @@
+'use strict';
+const User = function () {
+  let name = '';
+  Object.defineProperties(this, {
+    id: {
+      enumerable: true,
+      value: 'UID-${parseInt(Math.random() * 30000)}-XZ',
+      },
+    name: {
+      enumerable: true,
+      get() {
+        return name;
+        },
+      set(val) {
+        name = val;
+      },
+    },
+  });
+};
+let joe new User();
+joe.name = joe.name = 'Joe Mockery';
+onsole.log(joe.id, joe.name);
+
+        
+      
+      
+        
+      
+  
+    
+    
+      
