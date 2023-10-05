fb280878656744c2b31bbf0793656398b7fd30f5
Muhammad Raafid <140726193+MRaafid@users.noreply.github.com>
2023-10-05 19:58:31 +0530

    Create 5.js

---
+++ 5.js
@@ -0,0 +1,15 @@
+﻿const deepCopy = function (obj) {
+   let tempObj = {};
+   for (let key in obj) {
+     tempobj = {
+       ...tempObj,
+       [key]:
+        typeof obj[key] === 'object' && Array.isArray(obj [key])
+       ? {...deepCopy(obj [key]) }
+         : obj[key],
+     };
+   }
+   return tempobj;
+ };
+
+       
